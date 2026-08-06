b15470944f8832851af5d64412d29d3cb41be221
swathiga <swathiparamesh@gmail.com>
2026-08-06 09:11:53 +0530

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,14 @@
+let firstname = 'Swathiga'
+console.log('company name is',firstname,  'datatype is',typeof firstname);
+
+let companyName = 'Alight'
+console.log('company name is',companyName,  'datatype is',typeof companyName);
+
+let  mobilenumber = 12345
+console.log('mobilenumber  is',mobilenumber,  'datatype is',typeof mobilenumber);
+
+let isautomation = true
+console.log('automation  is',isautomation,  'datatype is',typeof isautomation);
+
+let hasplaywright
+console.log('playwright  is',hasplaywright,  'datatype is',typeof hasplaywright);
