94bc855092ae9c1e1b40024af051856e383f0636
swathiga <swathiparamesh@gmail.com>
2026-08-06 09:34:54 +0530

    Var_Let_Const classroom activity

---
+++ var_let_const.js
@@ -0,0 +1,13 @@
+const browserVersion = 'Chrome'
+
+function getBrowserVersion() {
+if(browserVersion=='Chrome')
+{
+   let browserVersion = 'Edge';
+   console.log('Browser version inside block is:',browserVersion);
+   
+}
+console.log('Browser version outside block is:',browserVersion);
+}
+getBrowserVersion()
+
